3403637f89ea9815aa97041574bafa26f7246bd6
JakeFenn <60349247+JakeFenn@users.noreply.github.com>
2020-05-21 22:24:11 +0100

    Add files via upload
    changed ball array to have one array which stores all moveable balls instead of spiltting

--- GameArena-master/Driver.java
+++ GameArena-master/Driver.java
@@ -5,17 +5,17 @@ public class Driver
 
     Ball white = new Ball(0,40,16,"WHITE",2);
     Ball black = new Ball(400,300,16,"BLACK",2);
-    Ball[] reds = new Ball[7];
-    Ball[] yellows = new Ball[7];
+    Ball[] playballs = new Ball[7];
+    Ball[] playballs = new Ball[7];
     Ball[] holes = new Ball[6];
     ga.addBall(white);
     //ga.addBall(black);
 
     /*for(int i=0;i<7;i++){
-      reds[i] = new Ball((100*i),200,16,"RED",2);
-      yellows[i] = new Ball((100*i),100,16,"YELLOW",2);
-      ga.addBall(reds[i]);
-      ga.addBall(yellows[i]);
+      playballs[i] = new Ball((100*i),200,16,"RED",2);
+      playballs[i] = new Ball((100*i),100,16,"YELLOW",2);
+      ga.addBall(playballs[i]);
+      ga.addBall(playballs[i]);
     }*/
 
     Rectangle table = new Rectangle(20,20,760,360,"GREEN",1);
